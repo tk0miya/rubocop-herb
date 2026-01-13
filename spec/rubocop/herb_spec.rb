415d63169ab9fb5242c6685dc9c230c7706f6e8c
Takeshi KOMIYA <i.tkomiya@gmail.com>
2026-01-14 04:15:58 +0900

    Set up rubocop

--- spec/rubocop/herb_spec.rb
+++ spec/rubocop/herb_spec.rb
@@ -2,10 +2,6 @@
 
 RSpec.describe Rubocop::Herb do
   it "has a version number" do
-    expect(Rubocop::Herb::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Rubocop::Herb::VERSION).not_to be_nil
   end
 end
